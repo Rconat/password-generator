cf5e79a14e286c80c4c8566b07eb40d2d1b30fda
ryzecous <r3j9c89@gmail.com>
2020-11-20 18:51:09 -0500

    layout structure of the script

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// User prompt for length of password
+// User Prompt for types of characters
+    // lowercase ?
+    // uppercase ?
+    // numeric ?
+    // special characters ?
+// check that at least 1 character type is selected
+
+// Generate the Password
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();

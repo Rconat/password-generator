0b5277fe66911d1855aab228563062585aed26b4
ryzecous <r3j9c89@gmail.com>
2020-11-21 18:36:43 -0500

    updating script

--- script.js
+++ script.js
@@ -1,14 +1,88 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// Add event listener to generate button
+generateBtn.addEventListener("click", generatePassword);
 
-  passwordText.value = password;
+// Global variables
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var special = ['#', '$', '%', '&', '(', ')', '*', '+', '<', '-', '.', '/', ':', ';', '<', '=', '>', '?', '!', '@', '[', ']'];
 
+var finalPassArray = []
+var finalPassword = ''
+
+var includeNumbers
+var includeUpper
+var includeLower
+var includeSpecial
+
+var randomNumbers
+var randomUppercase
+var randomLowercase
+var randomSpecial
+
+
+function generatePassword() {
+    var passPrompt = confirm('Create a new Password?');
+
+  if (passPrompt) {
+    var characterCount = parseInt(prompt('Enter a number between 8 and 128'))
+    console.log('the character count is ' + characterCount)
+    if (characterCount >= 8 && characterCount <= 128) {
+        // User Prompt for types of characters
+         // numeric ?
+        var includeNumbers = confirm('Include numbers?')
+        // special characters ?
+        var includeSpecial = confirm('Include special characters?')
+        // uppercase ?
+        var includeUppers = confirm('Include uppercase numbers?')
+        // lowercase ?
+        var includeLowers = confirm('Include lowercase numbers?')
+       
+        while (!(includeNumbers || includeSpecial || includeUppers || includeLowers)) {
+            alert("You must select at least one character type");
+            return
+        }
+
+        if (includeNumbers === true) {
+            console.log('including numbers')
+        } else {
+            console.log('not including numbers')
+        }
+
+        if (includeSpecial === true) {
+            console.log('including special characters')
+        } else {
+            console.log('not including special characters')
+        }
+
+        if (includeUppers === true) {
+            console.log('including uppers')
+        } else {
+            console.log('not including uppers')
+        }
+
+        if (includeLowers === true) {
+            console.log('including lowers')
+        } else {
+            console.log('not including lowers')
+        }
+
+    } else {
+        alert ("Password must be between 8 and 128 characters")
+    }
+  } else {
+      return
+  }
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
